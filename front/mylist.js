4e704c6894869f4f0896fecca1099ec3df5f9f52
mhty <olivergrane@gmail.com>
2016-04-10 18:40:46 +0300

    added drug'n'drop

--- front/mylist.js
+++ front/mylist.js
@@ -1,4 +1,5 @@
-myElements = [];
+var myElements = [];
+var dragElement = null;
 
 function newElement(line) {
     var e = {};
@@ -16,8 +17,31 @@ function add() {
 }
 
 function drawNewLine(e) {
+    var div = document.createElement("div");
+    div.id = "item_" + e.id;
+    div.innerHTML = e.text;
+    div.draggable = true;
     var li = document.createElement("li");
-    li.id = "item_" + e.id;
-    li.innerHTML = e.text;
+    li.appendChild(div);
+    li.addEventListener('dragstart', function(event) {
+          dragElement = this;
+        });
+    li.addEventListener('dragover', function(e) {
+                        		if (e.preventDefault) {
+                        			e.preventDefault();
+                        		}
+                        		e.dataTransfer.dropEffect = 'move';
+                        		return false;
+                        	});
+    li.addEventListener('drop', function(e) {
+                       		if (e.preventDefault) e.preventDefault();
+                         	if (e.stopPropagation) e.stopPropagation();
+                         	if (myElements.length > 1) {
+                                var list = document.getElementById("elementList")
+                                list.removeChild(dragElement);
+                       		    this.parentNode.insertBefore(dragElement, this);
+                       		}
+                       		return false;
+                       	});
     document.getElementById("elementList").appendChild(li);
 }
