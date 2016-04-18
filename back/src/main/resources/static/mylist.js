063301580dc40ee1104a268633495dd832234bb5
Dmitry Mikhaylovich <dmitry.mikhailovich@gmail.com>
2016-04-18 09:52:45 +0300

    fix order

--- back/src/main/resources/static/mylist.js
+++ back/src/main/resources/static/mylist.js
@@ -11,46 +11,68 @@ function add() {
     var line = document.getElementById("newElement").value
     document.getElementById("newElement").value = "";
     var e = newElement(line);
-    load();
-    myElements.push(e);
-    save();
-    //TODO fix reorder
-    drawLines();
+    load(e);
 }
 
 function createElement(id, text) {
     var div = document.createElement("div");
     div.id = id;
     div.innerHTML = $("<div>").text(text).html();
-    ;
-    var li = document.createElement("li");
-    li.draggable = true;
-    li.appendChild(div);
-    li.addEventListener('dragstart', function(e) {
+    div.draggable = true;
+    div.addEventListener('dragstart', function (e) {
         e.dataTransfer.setData("id", this.id);
-        e.dataTransfer.setData("text", this.innerHTML);
         e.dataTransfer.setData("type", "schhol-js-drag-and-drop");
         dragElement = this;
     });
-    li.addEventListener('dragover', function(e) {
+    div.addEventListener('dragover', function (e) {
         if (e.preventDefault) {
             e.preventDefault();
         }
         e.dataTransfer.dropEffect = 'move';
         return false;
     });
-    li.addEventListener('drop', function(e) {
+    div.addEventListener('drop', function (e) {
         if (e.preventDefault) e.preventDefault();
         if (e.stopPropagation) e.stopPropagation();
         if ((myElements.length > 1) && (e.dataTransfer.getData("type") == "schhol-js-drag-and-drop")) {
-            var list = document.getElementById("elementList")
-            list.removeChild(dragElement);
-            var li = createElement(e.dataTransfer.getData("id"), e.dataTransfer.getData("text"));
-            this.parentNode.insertBefore(li, this);
+            // здесь надо переделать, но задача работает
+            fromId = e.dataTransfer.getData("id");
+            toId = this.id;
+            from = 0;
+            to = 0;
+            myElements.forEach(function (value, index) {
+                if (value.id == fromId) {
+                    from = index;
+                }
+                if (value.id == toId) {
+                    to = index;
+                }
+            });
+            //
+            shift(from, to);
+            save();
+            drawLines();
         }
         return false;
     });
+    var li = document.createElement("li");
+    li.appendChild(div);
     return li;
+}
+
+function shift(from, to) {
+    var temp = myElements[from];
+    if (from < to) {
+        for (var i = from; i < to; i++) {
+            myElements[i] = myElements[+i + +1];
+        }
+    }
+    if (from > to) {
+        for (var i = from; i > to; i--) {
+            myElements[i] = myElements[i - 1];
+        }
+    }
+    myElements[to] = temp;
 }
 
 function drawNewLine(e) {
@@ -69,10 +91,12 @@ function save() {
     })
 }
 
-function load() {
+function load(e) {
     $.getJSON( "/load", function(data) {
         myElements = data;
+        myElements.push(e);
         drawLines();
+        save();
     });
 }
 
@@ -82,8 +106,11 @@ function drawLines() {
 }
 
 function sheduledLooad() {
-    load()
-    setTimeout(sheduledLooad, 10000);
+    $.getJSON("/load", function (data) {
+        myElements = data;
+        drawLines();
+    });
+    setTimeout(sheduledLooad, 3000);
 }
 
 window.onload = function () {
